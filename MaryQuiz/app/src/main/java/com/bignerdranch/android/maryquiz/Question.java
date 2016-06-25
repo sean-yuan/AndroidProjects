933e900832585a130889aafad420cef8e1268722
Sean Yuan <seanyuan@usc.edu>
2016-06-24 17:44:38 -0700

    Added modified game called MaryQuiz

---
+++ MaryQuiz/app/src/main/java/com/bignerdranch/android/maryquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.maryquiz;
+
+/**
+ * Created by seanyuan on 6/19/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+}
--- MaryQuiz/app/src/main/java/com/bignerdranch/android/maryquiz/QuizActivity.java
+++ MaryQuiz/app/src/main/java/com/bignerdranch/android/maryquiz/QuizActivity.java
@@ -1,10 +1,12 @@
-package com.bignerdranch.android.geoquiz;
+package com.bignerdranch.android.maryquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private TextView mPointsTextView;
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
 
@@ -26,11 +29,22 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private int mPoints = 0;
 
     private void updateQuestion(){
         //Log.d(TAG, "Updating question text for question #" + mCurrentIndex, new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();//access text id
         mQuestionTextView.setText(question);//set widget to current text id
+    }
+
+    private void updatePoints(Boolean answerCorrect){
+        if (answerCorrect){
+            mPoints++;
+        }
+        else{
+            mPoints--;
+        }
+        mPointsTextView.setText("Points: " + Integer.toString(mPoints));
     }
 
     private void checkAnswer(boolean userPressedTrue){
@@ -39,29 +53,43 @@ public class QuizActivity extends AppCompatActivity {
 
         if (userPressedTrue == answerIsTrue){
             messageResId = R.string.correct_toast;
+            updatePoints(true);
         }else{
             messageResId = R.string.incorrect_toast;
+            updatePoints(false);
         }
-
+        mCurrentIndex++;
+        if (mCurrentIndex+1 == mQuestionBank.length){//game finished
+            endGame();
+        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        updateQuestion();
+    }
+
+    private void endGame(){
+        LinearLayout ll = (LinearLayout) findViewById(R.id.question_layout);
+        ll.removeAllViews();
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
+        //Log.d(TAG, "onCreate(Bundle) called");
 
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);//access textview widget
-        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+        /*mQuestionTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
                 updateQuestion();
             }
-        });
+        });*/
+        mPointsTextView = (TextView) findViewById(R.id.points_text_view);
+        mPointsTextView.setText("Points: "+ Integer.toString(mPoints));
+
         //reference the inflated widget and cast as button
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
@@ -78,14 +106,14 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        /*mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View v){
                mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
                updateQuestion();
            }
-        });
+        });*/
         if(savedInstanceState != null){
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);//check if saved state exists
         }
